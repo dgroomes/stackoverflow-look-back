3560b74cb99cb3d69d63ad11b1c13eafd7976bae
David Groomes <dwgroomes@gmail.com>
2021-08-16 00:52:46 -0500

    Extract the result data
    This is scraping to the max! I know we could make a fetch request instead but
    reverse engineering the API is not as fun as scraping the HTML.

--- src/expand-posts.js
+++ src/expand-posts.js
@@ -28,7 +28,6 @@ let sql
  * Expand a list of post IDs into post objects by querying from the Stack Exchange Data Explorer
  *
  * @param {Array<Number>} ids the list of post ID
- * @return {Promise<Array<Post>>} the fully expanded Stack Overflow posts data
  */
 async function expand(ids) {
     console.log(`Querying for post information for posts with ids: ${ids}`)
@@ -38,8 +37,6 @@ async function expand(ids) {
     runQueryBtn.click() // Click the 'Run Query' button to prompt the parameters field to show up. The query is not actually run.
     document.querySelector('input[name=PostIds]').value = `'${ids}'` // Set the parameter
     runQueryBtn.click() // Run the query
-
-    // todo actually return the data
 }
 
 // This is the main function.
@@ -56,6 +53,30 @@ async function exec() {
         .then(response => response.json())
 
     console.log({votes})
+
+    // Before executing the SQL query, first register a MutationObserver to detect when the query results show up and
+    // to handle the results.
+    let queryResults = document.getElementById('resultSets');
+    let observer = new MutationObserver(mutations => {
+        console.log("SQL query results have arrived!")
+
+        // Get all row elements in the results table. The SEDE uses a JS library called SlickGrid to render tables. All
+        // rows have the class "slick-row".
+        let rows = document.querySelectorAll('#resultSets .slick-row')
+
+        // Look at the data...
+        for (let row of rows) {
+            // Extract each column of data
+            let id = row.querySelector("div.l0").innerText
+            let type = row.querySelector("div.l1").innerText
+            let title = row.querySelector("div.l2").innerText
+            let body = row.querySelector("div.l3").innerText
+            console.log({
+                id, type, title, body
+            })
+        }
+    })
+    observer.observe(queryResults, {subtree: true, childList: true})
 
     let ids = votes.map(vote => vote.postId)
     await expand(ids)
