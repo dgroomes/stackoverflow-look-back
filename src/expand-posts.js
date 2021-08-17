0faa914e5253b8686900e0d97a48b358ba3d6371
David Groomes <dwgroomes@gmail.com>
2021-08-16 20:43:33 -0500

    Proxy jQuery's ajax method to capture the query result sets
    This is nice. I want to Proxy XMLHttpRequest straight up next time.

--- src/expand-posts.js
+++ src/expand-posts.js
@@ -13,16 +13,97 @@ class Post {
     /**
      * @param id
      * @param type the type of post. Either "question" or "answer"
+     * @param title the title of the post. This is non-null for questions but is null for answers.
      * @param htmlBody the rendered HTML of the post body
      */
-    constructor(id, type, htmlBody) {
+    constructor(id, type, title, htmlBody) {
         this.id = id
         this.type = type
+        this.title = title
         this.htmlBody = htmlBody
     }
 }
 
 let sql
+let posts = []
+
+/**
+ * Instrument the jQuery object with our own custom interception code for the "ajax" function.
+ * To instrument jQuery we use a Proxy object. See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy.
+ *
+ * Note: I think this is mostly overkill. I could get the same effect without the Proxy object and instead just redefine
+ * the "ajax" method with a function that does the same thing. Proxy is awesome but might be overkill here. It and
+ * Reflect are a pretty good API.
+ */
+function instrumentJQuery() {
+
+    let handler = {
+        get: function (target, prop, receiver) {
+            console.log(`Property '${prop}' was accessed on the jQuery object ($)`)
+            let resolvedProp = Reflect.get(...arguments) // Get the "actual" property on the underlying proxied object.
+
+            if (prop === "ajax") {
+                console.log(`Instrumenting a pointcut/aspect around 'ajax'`)
+
+                return function instrumented() {
+                    console.log("'ajax' was invoked with: ")
+                    console.log({...arguments})
+
+                    // The 'ajax' function may take a variable amount of arguments. See the jQuery docs for the complete details: https://api.jquery.com/jquery.ajax/
+                    // But for our sake, we only expect it to be called with exactly one argument, the "settings" object.
+                    // Throw an error if the arguments do not meet our expectations.
+                    let argLength = arguments.length;
+                    if (argLength !== 1) {
+                        throw new Error(`Length of arguments of size '${argLength}' for the 'ajax' function was not expected.`)
+                    }
+                    let arg0 = arguments[0];
+                    let arg0Type = typeof arg0;
+                    if (arg0Type !== "object") { // Technically this is not a perfect assertion. But it's good enough. We mostly want to assert that the argument is not a string but is instead an object. (Strings aren't objects in JavaScript.)
+                        throw new Error(`Expected a string but found type '${arg0Type}' for argument '${arg0}' to the 'ajax' function`)
+                    }
+
+                    let settings = arg0 // arg0 is the "settings" object that the "ajax" function takes. See https://api.jquery.com/jquery.ajax/
+                    let {success} = settings
+                    if (!success) {
+                        throw new Error("There was no 'success' callback defined in the settings for the ajax request. This is unexpected.")
+                    }
+
+                    // Proxy the "success" callback so that we can intercept the result set of successful queries to the Stack Exchange Data Explorer
+                    settings.success = new Proxy(success, {
+                        apply(target, thisArg, argumentsList) {
+                            console.log("The 'success' callback was invoked on this ajax request.")
+
+                            let responseData = argumentsList[0];
+
+                            // Get the first element in the result sets array. When would this ever be more than one?
+                            let {rows} = responseData.resultSets[0]
+
+                            // Collect the post data
+                            for (let row of rows) {
+                                let id = row[0]
+                                let type = row[1]
+                                let title = row[2]
+                                let body = row[3]
+                                posts.push(new Post(id, type, title, body))
+                            }
+
+                            console.log({posts})
+
+                            // Finally, delegate to the underlying "original/normal/actual" function.
+                            Reflect.apply(...arguments)
+                        }
+                    })
+
+                    return resolvedProp.bind(receiver)(...arguments) // Invoke the "actual" property
+                }
+            }
+            return resolvedProp
+        }
+    }
+    let proxy = new Proxy(window.$, handler);
+    window.$ = proxy
+    window.jQuery = proxy
+}
 
 /**
  * Expand a list of post IDs into post objects by querying from the Stack Exchange Data Explorer
@@ -54,29 +135,7 @@ async function exec() {
 
     console.log({votes})
 
-    // Before executing the SQL query, first register a MutationObserver to detect when the query results show up and
-    // to handle the results.
-    let queryResults = document.getElementById('resultSets');
-    let observer = new MutationObserver(mutations => {
-        console.log("SQL query results have arrived!")
-
-        // Get all row elements in the results table. The SEDE uses a JS library called SlickGrid to render tables. All
-        // rows have the class "slick-row".
-        let rows = document.querySelectorAll('#resultSets .slick-row')
-
-        // Look at the data...
-        for (let row of rows) {
-            // Extract each column of data
-            let id = row.querySelector("div.l0").innerText
-            let type = row.querySelector("div.l1").innerText
-            let title = row.querySelector("div.l2").innerText
-            let body = row.querySelector("div.l3").innerText
-            console.log({
-                id, type, title, body
-            })
-        }
-    })
-    observer.observe(queryResults, {subtree: true, childList: true})
+    instrumentJQuery()
 
     let ids = votes.map(vote => vote.postId)
     await expand(ids)
