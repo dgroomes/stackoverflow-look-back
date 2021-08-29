7fb8013dad4d6d0cab53c40c2e4c8db5bf3cd185
David Groomes <dwgroomes@gmail.com>
2021-08-29 14:45:45 -0500

    Create 'execContentScript' function that abstracts between manifest v2/v3

--- src/extension/common/extension-entrypoint.js
+++ src/extension/common/extension-entrypoint.js
@@ -4,16 +4,38 @@
 // This code sets up the infrastructure for listening to messages from the web page. This code bootstraps the content
 // script which then bootstraps the web page.
 
-async function bootstrapTheContentScript() {
 
-    // Get the current tab
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+/**
+ * Execute a content script.
+ *
+ * Special care is taken to abstract between the Manifest V2 and V3 APIs.
+ * See the contrast between how to execute scripts between the Manifest V2 and V3 APIs: https://developer.chrome.com/docs/extensions/mv3/intro/mv3-migration/#executing-arbitrary-strings
+ *
+ * @param fileName the file name of the content script
+ * @return {Promise} that resolves when the content script has been loaded/executed(?)
+ */
+async function execContentScript(fileName) {
 
-    // Create the content script entrypoint
-    await chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ["extension/common/content-script-entrypoint.js"]
-    })
+    // Detect which version of the manifest this extension is defined as (e.g. Manifest V2 or V3)
+    // The value will be a number: either 2 or 3.
+    let manifestVersion = chrome.runtime.getManifest().manifest_version
+
+    if (manifestVersion === 2) {
+        chrome.tabs.executeScript({
+            file: fileName
+        })
+    } else if (manifestVersion === 3) {
+        // Get the current tab
+        let [tab] = await chrome.tabs.query({active: true, currentWindow: true})
+
+        // Create the content script entrypoint
+        await chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            files: [fileName]
+        })
+    } else {
+        console.error(`Chrome manifest version not recognized: ${manifestVersion}`)
+    }
 }
 
 // Register a listener for message passing to the web page.
@@ -25,12 +47,12 @@ chrome.runtime.onMessageExternal.addListener(async function (message, sender, se
         let data = message.data
         chrome.storage.local.set(data, () => {
             sendResponse("The extension successfully saved the data")
-        });
+        })
     } else if (message.command === "get") {
         let key = message.key
         chrome.storage.local.get(key, (found) => {
             sendResponse(found)
-        });
+        })
     } else if (message.command === "open-generate-html-page") {
         chrome.tabs.create({
             url: 'web/generate-html.html'
@@ -40,6 +62,6 @@ chrome.runtime.onMessageExternal.addListener(async function (message, sender, se
     }
 })
 
-bootstrapTheContentScript()
-    .then(() => console.log("[extension-entrypoint.js#bootstrapTheContentScript] Ran to completion"))
-    .catch(e => console.log("[extension-entrypoint.js#bootstrapTheContentScript] Something went wrong", e))
+execContentScript("extension/common/content-script-entrypoint.js")
+    .then(() => console.log("[extension-entrypoint.js] Ran to completion"))
+    .catch(e => console.log("[extension-entrypoint.js] Something went wrong", e))
