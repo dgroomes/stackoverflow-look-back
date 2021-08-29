dc50c67b3d78be5af77cfffc528db056d3791f0d
David Groomes <dwgroomes@gmail.com>
2021-08-29 14:20:25 -0500

    Add the page_action to the manifest v2 project

--- README.md
+++ README.md
@@ -270,4 +270,5 @@ These are the finished items from the Wish List:
 * [FireFox Extension Workshop: *Porting a Google Chrome
   extension*](https://extensionworkshop.com/documentation/develop/porting-a-google-chrome-extension/)
     * Shoot, FireFox doesn't support Manifest v3 and I spent all this time writing a Chrome extension in Manifest v3. I
-      wish I had implemented in Manifest v2 so that I could compatibility with FireFox. 
+      wish I had implemented in Manifest v2 so that I could compatibility with FireFox.
+* [Chrome extension docs: Manifest V2 *Getting started*](https://developer.chrome.com/docs/extensions/mv2/getstarted/)
--- src/extension/manifest-v2/init.js
+++ src/extension/manifest-v2/init.js
@@ -1,9 +1,8 @@
 // Core extension initialization code
 //
-// This code initializes some configuration values.
-//
-// Note that this file must be in the web extension's root directory (the same directory as the manifest.json file) because
-// of the way Chrome service workers work. See the "Gotchas" note at https://developer.chrome.com/docs/extensions/mv3/mv3-migration-checklist/
+// This code initializes some configuration values and declares the permissions that dictate which pages the extension
+// are enabled for. This is a key difference between the extension architectures of Manifest V2 and V3. In V3, we
+// declares these permissions in the manifest file itself.
 
 const defaultInitialVotesPageLimit = 1 // a default value for the "votes page limit" configuration.
 
@@ -11,4 +10,14 @@ chrome.runtime.onInstalled.addListener(async () => {
     chrome.storage.local.set({votesPageLimit: defaultInitialVotesPageLimit}, () => {
         console.log(`[init.js] Saved default value for 'votesPageLimit': '${defaultInitialVotesPageLimit}'`);
     })
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {hostEquals: 'stackoverflow.com'},
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 })
